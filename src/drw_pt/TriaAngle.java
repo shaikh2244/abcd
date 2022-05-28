5c5f940143a7f0e9acca7ecac0e001b49963a2ee
shaikh2244 <106425668+shaikh2244@users.noreply.github.com>
2022-05-28 16:32:48 +0530

    Add files via upload

---
+++ src/drw_pt/TriaAngle.java
@@ -0,0 +1,23 @@
+package drw_pt;
+
+public class TriaAngle 
+{
+	public static void main(String[] args)
+	{
+		for(int i=1; i<=5; i++)
+		{
+			for(int j=i; j<=5; j++)
+			{
+				System.out.print("  ");
+			}
+			
+			{
+				System.out.print("* ");
+			}
+			for(int j=1; j<=i; j++)
+			{
+		        System.out.println();
+			}
+}
+	}
+}
